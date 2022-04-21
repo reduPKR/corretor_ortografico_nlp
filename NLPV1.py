2137000e5496685530be0d1f6123d6eae4c6d873
Rafael Eduardo <56879793+reduPKR@users.noreply.github.com>
2022-04-20 22:23:25 -0300

    criação de corretor por probabilidade

--- NLPV1.py
+++ NLPV1.py
@@ -3,19 +3,27 @@ import nltk as nltk
 
 # nltk.download('punkt')
 
+
 class NLPV1:
+    total_palavras = 0
+    frequencia = []
     def abrir_arquivo(self):
         with open("dados/artigos.txt", "r", encoding="utf-8") as arq:
             artigos = arq.read()
 
             tokens = nltk.tokenize.word_tokenize(artigos)
             texto = self.separar_palavras(tokens)
-            normalizada = self.normalizar(texto)
-            palavras = self.remover_palavras_repetidas(normalizada)
 
+            normalizada = self.normalizar(texto)
+            self.total_palavras = len(normalizada)
+            self.frequencia = nltk.FreqDist(normalizada)
+
+            palavras = self.remover_palavras_repetidas(normalizada)
 
             print(f"Total de tokens {len(tokens)}")
             print(f"Total de palavras {len(palavras)}")
+
+            print(self.corretor('lógia'))
 
 
     def separar_palavras(self, tokens):
@@ -35,5 +43,35 @@ class NLPV1:
 
         return normalizada
 
-    def remover_palavras_repetidas(self, palavras):
-       return set(palavras)
+    def remover_palavras_repetidas(self,normalizada):
+       return set(normalizada)
+
+    def corretor(self,palavra):
+        variantes = self.gerar_palavras(palavra)
+        print(variantes)
+
+        #retorna a variante com maior probalidade de ser a palavra correta
+        return max(variantes, key=self.calcular_propabilidade)
+
+
+    def gerar_palavras(self, palavra):
+        fatias = []
+        palavras = []
+        for i in range(len(palavra)+1):
+            fatias.append((palavra[:i], palavra[i:]))
+            palavras = self.insere_letras(fatias)
+
+        return palavras
+
+    def insere_letras(self,fatias):
+        novas_palavras = []
+
+        letras = 'abcdefghijklmnopqrstuvwxyzàáâãèéêìíîòóôõùúûç'
+        for esquerdo, direito in fatias:
+            for letra in letras:
+                novas_palavras.append(esquerdo + letra + direito)
+
+        return novas_palavras
+
+    def calcular_propabilidade(self, variante):
+        return self.frequencia[variante] / self.total_palavras
